3a1bfb472d27a21a8f2a87bdc05aca1ad84469c5
KarimmYasser <Karimmyasserr@gmail.com>
2023-12-26 23:04:59 +0200

    adding Comments

--- Actions/ClearAll.cpp
+++ Actions/ClearAll.cpp
@@ -4,14 +4,14 @@ ClearAll::ClearAll(ApplicationManager* pApp) : Action(pApp) {}
 void ClearAll::ReadActionParameters(){}
 void ClearAll::Execute()
 {
-	if (pManager->getIsRecording())
+	if (pManager->getIsRecording()) //check if recording is started or not to stop it
 		pManager->StopRecord();
-	ClearRecord.open("Last Record", ios::trunc);
+	ClearRecord.open("Last Record", ios::trunc); //clear recording
 	ClearRecord.close();
-	pManager->Clearall();
-	pManager->ClearRedoList();
-	pManager->ClearUndoList();
-	pManager->ResetConstants();
+	pManager->Clearall(); //clear all figures
+	pManager->ClearRedoList(); //clear redo history
+	pManager->ClearUndoList(); //clear undo history
+	pManager->ResetConstants(); //reset UI constants
 }
 
 Action* ClearAll::Clone()
